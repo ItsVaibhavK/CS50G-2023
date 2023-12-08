e92d7c8ba688b5f85c19c5fadcb167a35ce3a8a5
Vaibhav <120876537+ItsVaibhavK@users.noreply.github.com>
2023-12-09 00:47:13 +0530

    Week 1 - Flappy Bird

---
+++ flappy-bird/states/ScoreState.lua
@@ -0,0 +1,56 @@
+--[[
+    ScoreState Class
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    A simple state used to display the player's score before they
+    transition back into the play state. Transitioned to from the
+    PlayState when they collide with a Pipe.
+]]
+
+ScoreState = Class{__includes = BaseState}
+
+--[[
+    When we enter the score state, we expect to receive the score
+    from the play state so we know what to render to the State.
+]]
+
+-- medal images
+-- credit/source: https://grappe.itch.io/medal
+bronze = love.graphics.newImage('bronze.png')
+silver = love.graphics.newImage('silver.png')
+gold = love.graphics.newImage('gold.png')
+
+function ScoreState:enter(params)
+    self.score = params.score
+end
+
+function ScoreState:update(dt)
+    -- go back to play if enter is pressed
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+
+end
+
+function ScoreState:render()
+    -- simply render the score to the middle of the screen
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+
+    -- medals displayed according to player's score
+    if self.score < 2 then
+        love.graphics.draw(bronze, 215, 105, 0, 0.1, 0.1)
+    end
+    if self.score >= 2 and self.score < 4 then
+        love.graphics.draw(silver, 215, 105, 0, 0.1, 0.1)
+    end
+    if self.score >= 4 then
+        love.graphics.draw(gold, 215, 105, 0, 0.1, 0.1)
+    end
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+end
